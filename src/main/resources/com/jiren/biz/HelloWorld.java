45187eb0d432fe047369578119975f850e1615b3
GongWei <blackjackhoho@gmail.com>
2014-06-04 18:30:31 +0800

    first

---
+++ src/main/resources/com/jiren/biz/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.jiren.biz;
+
+/**
+ * Created by konbluesky on 14-6-4.
+ */
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.print("adfdsf");
+    }
+}
